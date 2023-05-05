36e4b6699e33429c6212b6b4bda573d97860f508
nicholas <nnnnicholas@users.noreply.github.com>
2023-05-05 13:14:38 -0400

    typo

--- src/MyTokenResolver.sol
+++ src/MyTokenResolver.sol
@@ -11,7 +11,6 @@ contract MyTokenResolver is IJBTokenUriResolver {
         uri = _uri;
     }
     
-    // Note: this function is marked `pure` because it does not mutate chain state. The ERC721 standard uses `view` instead (source: https://eips.ethereum.org/EIPS/eip-721).
     function getUri(uint256 _projectId) external view returns (string memory tokenUri) {
         return uri;
     }
