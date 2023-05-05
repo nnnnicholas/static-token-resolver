f046136c932148089b4af2e950e62b5803c838b9
nicholas <nnnnicholas@users.noreply.github.com>
2023-05-05 12:35:26 -0400

    Bare essentials resolver template created

---
+++ src/MyTokenResolver.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import {IJBTokenUriResolver} from "@jbx-protocol/juice-contracts-v3/contracts/interfaces/IJBTokenUriResolver.sol";
+
+contract MyTokenResolver {
+
+    string public uri;
+
+    constructor(string memory  _uri) {
+        uri = _uri;
+    }
+
+    
+    // Note: this function is marked `pure` because it does not mutate chain state. The ERC721 standard uses `view` instead (source: https://eips.ethereum.org/EIPS/eip-721).
+    function getUri(uint256 _projectId) external view returns (string memory tokenUri) {
+        return uri;
+    }
+}
---
+++ test/MyTokenResolverTest.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "forge-std/Test.sol";
+import "src/MyTokenResolver.sol";
+
+contract MyTokenResolverTest is Test {
+
+    function setUp() public {}
+
+    function testOne() public {
+    }
+}
